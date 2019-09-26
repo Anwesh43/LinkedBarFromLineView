e347c03d9b7aef5e44b2695e196718a62ad8e0ea
Anwesh43 <anweshthecool0@gmail.com>
2019-09-26 10:08:53 +0530

    adding BarFromLineView class

--- barfromlineview/src/main/java/com/anwesh/uiprojects/barfromlineview/BarFromLineView.kt
+++ barfromlineview/src/main/java/com/anwesh/uiprojects/barfromlineview/BarFromLineView.kt
@@ -66,3 +66,21 @@ fun Canvas.drawBFLNode(i : Int, scale : Float, paint : Paint) {
     drawBarLine(size, sc1, paint)
     restore()
 }
+
+class BarFromLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
