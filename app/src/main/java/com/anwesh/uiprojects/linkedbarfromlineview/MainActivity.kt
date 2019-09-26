7024da6ed263e2141d204a9beb121d54a4c3fc80
Anwesh43 <anweshthecool0@gmail.com>
2019-09-26 10:38:31 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbarfromlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbarfromlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbarfromlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.barfromlineview.BarFromLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BarFromLineView.create(this)
     }
 }
