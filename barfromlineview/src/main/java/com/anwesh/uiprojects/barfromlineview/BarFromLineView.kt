2ba5306e8ac834961d626ffeef7221bb7045d95a
Anwesh43 <anweshthecool0@gmail.com>
2019-09-26 10:17:51 +0530

    adding BFLNode class

--- barfromlineview/src/main/java/com/anwesh/uiprojects/barfromlineview/BarFromLineView.kt
+++ barfromlineview/src/main/java/com/anwesh/uiprojects/barfromlineview/BarFromLineView.kt
@@ -131,4 +131,46 @@ class BarFromLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BFLNode(var i : Int, val state : State = State()) {
+
+        private var next : BFLNode? = null
+        private var prev : BFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBFLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BFLNode {
+            var curr : BFLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
