4e6153cc4d426e8f5dc5c909af875e68fa722ddb
Anwesh43 <anweshthecool0@gmail.com>
2019-09-26 10:33:57 +0530

    adding Renderer class

--- barfromlineview/src/main/java/com/anwesh/uiprojects/barfromlineview/BarFromLineView.kt
+++ barfromlineview/src/main/java/com/anwesh/uiprojects/barfromlineview/BarFromLineView.kt
@@ -197,4 +197,26 @@ class BarFromLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BarFromLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val bfl : BarFromLine = BarFromLine(0)
+
+        fun render(canvas : Canvas, paint : Paint) {
+            canvas.drawColor(backColor)
+            bfl.draw(canvas, paint)
+            animator.animate {
+                bfl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bfl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
